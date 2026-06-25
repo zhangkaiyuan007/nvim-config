8fd4e20a9f6096ca2f3ea240856a2201b4c068ee
kyzhang <robert_kaiyuan@outlook.com>
2026-06-26 04:06:42 +0800

    use yazi instead

---
+++ lua/kyzhang/lazy/yazi.lua
@@ -0,0 +1,21 @@
+return {
+  "mikavilpas/yazi.nvim",
+  version = "*",
+  event = "VeryLazy",
+  dependencies = {
+    { "nvim-lua/plenary.nvim", lazy = true },
+  },
+  keys = {
+    {
+      "<leader>e",
+      mode = { "n", "v" },
+      "<cmd>Yazi<cr>",
+      desc = "Open yazi at the current file",
+    },
+    {
+      "<c-up>",
+      "<cmd>Yazi toggle<cr>",
+      desc = "Resume the last yazi session",
+    },
+  },
+}
